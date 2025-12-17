170890f062b17b0803b028468f5b4faff22c93d8
mc4024 <mertdabaus@gmail.com>
2025-12-17 16:31:47 +0000

    FEAT: implemented thread pool

--- src/headers/thread_pool.h
+++ src/headers/thread_pool.h
@@ -1,16 +1,25 @@
-//
-// Created by Mertd on 12/17/2025.
-//
+#ifndef THREAD_POOL_HPP
+#define THREAD_POOL_HPP
 
-#ifndef THREAD_POOL_H
-#define THREAD_POOL_H
+#include <vector>
+#include <thread>
 
-
+#include "task_queue.h"
+#include "headers/task_queue.h"
 
 class thread_pool {
+public:
+    thread_pool(task_queue& queue, size_t num_threads);
 
+    ~thread_pool();
+
+    // prevents copying of the thread pool
+    thread_pool(const thread_pool&) = delete;
+    thread_pool& operator=(const thread_pool&) = delete;
+
+private:
+    std::vector<std::thread> workers;
+    task_queue& target_queue;
 };
 
-
-
-#endif //THREAD_POOL_H
+#endif
--- src/thread_pool.cpp
+++ src/thread_pool.cpp
@@ -1,5 +1,32 @@
-//
-// Created by Mertd on 12/17/2025.
-//
-
 #include "headers/thread_pool.h"
+#include <iostream>
+
+thread_pool::thread_pool(task_queue& queue, size_t num_threads) : target_queue(queue) {
+    for (size_t i = 0; i < num_threads; ++i) {
+        workers.emplace_back([this, i] {
+            while (true) {
+                int client_fd = target_queue.pop();
+
+                if (client_fd == -1) {
+                    break;
+                }
+
+                std::cout << "[Worker " << i << "] Handling connection on FD: "
+                          << client_fd << std::endl;
+
+                // TODO: In the future, this is where worker.cpp logic goes
+                // handle_client(client_fd);
+            }
+        });
+    }
+}
+
+thread_pool::~thread_pool() {
+    target_queue.shutdown();
+
+    for (std::thread &worker : workers) {
+        if (worker.joinable()) {
+            worker.join();
+        }
+    }
+}
